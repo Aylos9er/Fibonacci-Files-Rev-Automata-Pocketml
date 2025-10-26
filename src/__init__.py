e5ce0855d0d8eebb0c348c421d2852d5119964e6
j Mosij <mosij@icloud.com>
2025-10-26 09:57:11 -0700

    Fix src/__init__.py - Correct packaging and documentation

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Fibonacci Reversible Automata Package
+Reversible Turing-complete cellular automaton with Fibonacci spiral geometry
+
+Author: j Mosij (mosij@icloud.com)
+"""
+
+from .fibonacci_automata import ReversePineConeFibonacciAutomata
+
+__version__ = "1.0.0"
+__author__ = "j Mosij"
+__email__ = "mosij@icloud.com"
+
+__all__ = ['ReversePineConeFibonacciAutomata']
